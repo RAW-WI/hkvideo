df1b97849860b55f10e49e512ff72d4860d77d8a
mrchai <630674409@qq.com>
2024-07-02 08:51:33 +0800

    完成入门案例所有代码，实现登录后台逻辑

--- day02/springboot-demo/src/main/java/com/softeem/springbootdemo/controller/IndexController.java
+++ day02/springboot-demo/src/main/java/com/softeem/springbootdemo/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.softeem.springbootdemo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.softeem.springbootdemo.dao.UserDAO;
 import com.softeem.springbootdemo.dto.Result;
 import com.softeem.springbootdemo.entity.User;
@@ -27,7 +29,26 @@ public class IndexController {
     public Map<String,Object> login(String username, String password){
         //创建Map集合对象，用于传递丰富的数据到前端
         Map<String,Object> map = new HashMap<>();
-        //todo 实现连接数据库登录逻辑？
+        //创建查询条件包装器
+        QueryWrapper<User> query = Wrappers.query(User.class);
+        //username=?
+        query.eq("username",username);
+        // 1.根据用户名查询用户对象（返回一个用户对象）  select * from user where username=?
+        User user = userDAO.selectOne(query);
+        // 2.判断用户是否存在
+        if(Objects.isNull(user)){
+            map.put("code",-1);
+            map.put("msg", "用户不存在");
+        }else{
+            // 3.如果存在则判断密码是否正确，不存在直接返回错误结果
+            if(Objects.equals(user.getPassword(),password)){
+                map.put("code",1);
+                map.put("msg","登录成功");
+            }else{
+                map.put("code",0);
+                map.put("msg","密码错误");
+            }
+        }
         return map;
     }
 
